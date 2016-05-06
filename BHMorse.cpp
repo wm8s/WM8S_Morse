0ff4ad06adb955bd8a8c20703a6d5c49536ae602
Rob Bailey <rob@wm8s.com>
2016-05-06 01:19:49 -0500

    Added files via upload

--- BHMorse.cpp
+++ BHMorse.cpp
@@ -2,10 +2,27 @@
 //
 //	BHMorse.cpp - Library for Morse code
 //
-//	Created 2016-04-01 by Rob Bailey, WM8S
+//	Copyright © 2016 Robert L. Bailey
 //
 //	Send Morse Code, including generating random characters, groups of
 //	characters, or plausible QSOs.
+//
+//////////////////////////////////////////////////////////////////////////////
+//
+//	This file is part of BHMorse.
+//
+//	BHMorse is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	BHMorse is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with BHMorse.  If not, see <http://www.gnu.org/licenses/>.
 //
 //////////////////////////////////////////////////////////////////////////////
 
--- BHMorse.h
+++ BHMorse.h
@@ -2,7 +2,24 @@
 //
 //	BHMorse.h - Library for Morse code
 //
-//	Created 2016-04-01 by Rob Bailey, WM8S
+//	Copyright © 2016 Robert L. Bailey
+//
+//////////////////////////////////////////////////////////////////////////////
+//
+//	This file is part of BHMorse.
+//
+//	BHMorse is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//
+//	BHMorse is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License
+//	along with BHMorse.  If not, see <http://www.gnu.org/licenses/>.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
